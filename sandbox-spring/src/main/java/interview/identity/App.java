bbe8addad376619a14fda749e213744630dc7af1
Ken Lewis <klewisjr@hotmail.com>
2025-11-25 23:21:20 -0800

    init

---
+++ sandbox-spring/src/main/java/interview/identity/App.java
@@ -0,0 +1,11 @@
+package interview.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
---
+++ sandbox-spring/src/main/java/interview/identity/controller/HelloController.java
@@ -0,0 +1,13 @@
+package interview.identity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+
+  @GetMapping("/hello")
+  String hello() {
+    return "ok";
+  }
+}
